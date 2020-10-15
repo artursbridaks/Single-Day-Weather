42ab0fbd5b02ae9c077a3c935da2aea88fbf8b8d
Arturs <bridaks.arturs@gmail.com>
2020-10-15 12:39:23 +0300

    Single day weather

---
+++ README.md
@@ -0,0 +1,26 @@
+# Single Day Weather App
+
+Weather forecast app which uses the OpenWeatherMap API. Built using Vue framework.
+
+## Visual representation
+
+![Day-Weather-edit](https://user-images.githubusercontent.com/48471924/96101418-199a4000-0ede-11eb-80a6-e3672a57cd47.gif)
+
+## Run
+Clone the project to your local machine and run:
+
+```
+npm i
+```
+
+## Start the server
+```
+npm run serve
+```
+
+### Tech Stack
+
+* Vue.js
+* JavaScript (ES6)
+* HTML
+* CSS
---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          autocomplete="on"
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ getDate() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{ weather.weather[0].description }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "3caad0ce38c6e25179a142eedf3d7833",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    getDate() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+
+      return `${day}, ${month} ${date}`;
+    },
+  },
+};
+</script>
+
+<style>
+@import "./App.css";
+</style>
